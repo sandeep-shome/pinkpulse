d830f2e6441d0b51c709ec0f0af1809ef03beae6
sandeep <sandeepshome10@gmail.com>
2024-04-13 21:34:27 +0530

    fix: loading state

--- client/src/screens/login.jsx
+++ client/src/screens/login.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Button, ToastAction, useToast } from "@/components/ui";
 import { googleImg } from "@/assets";
@@ -7,14 +7,19 @@ import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../firebase.config";
 import axios from "axios";
 import { UserContext } from "@/context/userContext";
+import Loader from "@/components/created/loader";
 
 const Login = () => {
   const { toast } = useToast();
   const navigate = useNavigate();
   const userContext = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
+  const buttonRef = useRef(null);
 
   //SIGN IN WITH GOOGLE
   const handleSignin = () => {
+    setLoading(true);
+    buttonRef.current.setAttribute("disabled", "");
     try {
       signInWithPopup(auth, provider).then((res) => {
         const user = res.user;
@@ -42,6 +47,8 @@ const Login = () => {
             navigate("/periods");
           })
           .catch(() => {
+            setLoading(false);
+            buttonRef.current.removeAttribute("disabled");
             toast({
               variant: "destructive",
               title: "Uh oh! Something went wrong.",
@@ -51,6 +58,8 @@ const Login = () => {
           });
       });
     } catch (error) {
+      setLoading(false);
+      buttonRef.current.removeAttribute("disabled");
       toast({
         variant: "destructive",
         title: "Uh oh! Something went wrong.",
@@ -61,29 +70,33 @@ const Login = () => {
   };
 
   return (
-    <div className="w-full flex items-center justify-center flex-col">
-      <Link to={"/"} className="flex items-center justify-center gap-1 mb-3">
-        <GiFairy className="text-4xl text-pink-400" />
-        <p className="text-xl text-pink-400">PinkPulse</p>
-      </Link>
-      <h1 className="text-center mb-2 text-xl text-slate-700">
-        Sign in your account
-      </h1>
-      <p className="mb-5 text-slate-500 text-center">
-        Use your google account for social signin, we currently only available
-        for google
-      </p>
-      <Button
-        className="flex items-center justify-center gap-2 max-sm:w-[90%] text-base mb-5"
-        onClick={handleSignin}
-      >
-        <img src={googleImg} alt="" className="w-5 h-5 " size="lg" />
-        Sign in with Google
-      </Button>
-      <span className="text-center text-sm text-slate-500">
-        By clicking you are accepting our privacy policy
-      </span>
-    </div>
+    <>
+      {loading && <Loader />}
+      <div className="w-full flex items-center justify-center flex-col">
+        <Link to={"/"} className="flex items-center justify-center gap-1 mb-3">
+          <GiFairy className="text-4xl text-pink-400" />
+          <p className="text-xl text-pink-400">PinkPulse</p>
+        </Link>
+        <h1 className="text-center mb-2 text-xl text-slate-700">
+          Sign in your account
+        </h1>
+        <p className="mb-5 text-slate-500 text-center">
+          Use your google account for social signin, we currently only available
+          for google
+        </p>
+        <Button
+          className="flex items-center justify-center gap-2 max-sm:w-[90%] text-base mb-5"
+          onClick={handleSignin}
+          ref={buttonRef}
+        >
+          <img src={googleImg} alt="" className="w-5 h-5 " size="lg" />
+          Sign in with Google
+        </Button>
+        <span className="text-center text-sm text-slate-500">
+          By clicking you are accepting our privacy policy
+        </span>
+      </div>
+    </>
   );
 };
 
